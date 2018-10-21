c5381987f9f72c49a3f491730afe2064df00d584
Hitoshi Okawa <14all.okawa@gmail.com>
2018-10-21 17:47:34 +0900

    Resolve path of schema.graphql file

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import "reflect-metadata";
 import { importSchema } from 'graphql-import';
 import { GraphQLServer } from 'graphql-yoga';
 import { resolvers } from './resolvers';
+import * as path from 'path';
 
-const typeDefs = importSchema("schema.graphql");
+const typeDefs = importSchema(path.join(__dirname, "schema.graphql"));
 
 const server = new GraphQLServer({ typeDefs, resolvers })
 server.start(() => console.log('Server is running on localhost:4000'))
