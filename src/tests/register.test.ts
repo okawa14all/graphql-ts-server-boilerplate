e102002d8274053d2c948b16fbecfa27b01dcec1
大川仁志 <hitoshi.okawa.biz@gmail.com>
2018-11-06 09:42:34 +0900

    Fix test

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -8,7 +8,7 @@ let getHost = () => '';
 beforeAll(async () => {
   const app = await startServer();
   const { port } = app.address() as AddressInfo;
-  getHost = () => `http:127.0.0.1:${port}`;
+  getHost = () => `http://127.0.0.1:${port}`;
 });
 
 const email = 'bob@bob.com';
