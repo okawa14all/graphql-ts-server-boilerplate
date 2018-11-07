00b45119dc7966a8ac40b5aeb5f2d58b974c01cc
大川仁志 <hitoshi.okawa.biz@gmail.com>
2018-11-07 09:43:36 +0900

    Schema stitching

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,12 +1,22 @@
 import { importSchema } from 'graphql-import';
 import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLSchema } from 'graphql';
+import { mergeSchemas, makeExecutableSchema } from 'graphql-tools';
 import * as path from 'path';
-import { resolvers } from './resolvers';
+import * as fs from 'fs';
 import { createTypeormConn } from './utils/createTypeormConn';
 
 export const startServer = async () => {
-  const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
-  const server = new GraphQLServer({ typeDefs, resolvers });
+  const folders = fs.readdirSync(path.join(__dirname, './modules'));
+  const schemas: GraphQLSchema[] = folders.map(folder => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    return makeExecutableSchema({ resolvers, typeDefs })
+  })
+
+  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
   await createTypeormConn();
   const app = await server.start({ port: process.env.NODE_ENV === 'test' ? 0 : 4000 });
   console.log('Server is running on localhost:4000');
